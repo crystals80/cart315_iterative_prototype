56ff2c80638649434cc48b9a6522e2207a130f63
Lam Ky Anh Do <faleanh@hotmail.com>
2023-04-17 01:20:49 -0400

    Fixed Health system
    Made it work: everytime the first controller collide with a gameobject (obstacle), it loses a heart
    Updated Levels
    Clean up code

--- Assets/Damage.cs
+++ Assets/Damage.cs
@@ -1,23 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Damage : MonoBehaviour
 {
+    // Variable for damage control
     [SerializeField] private int obstacleDamage;
-    [SerializeField] private Health _healthControl;
+    [SerializeField] private GameObject _gameManager;
 
-    private void OnTriggerEnter(Collider collision)
+    // Variables for Teleport and checkpoints
+    public GameObject Player;
+    public GameObject ToCheckpoint0;
+    public GameObject ToCheckpoint1;
+    public GameObject ToCheckpoint2;
+    public GameObject ToCheckpoint3;
+
+    void Start()
     {
-        if (collision.CompareTag("Player"))
+        Debug.Log("hello");
+        obstacleDamage = 1;
+    }
+
+    private void OnCollisionEnter (Collision target)
+    {
+        Debug.Log(target.gameObject.name.ToString());
+        if (target.gameObject.name.ToString().StartsWith("Obstacle"))
         {
+            Debug.Log("message");
             DamageControl();
-        }  
+
+            if (target.gameObject.CompareTag("Untagged"))
+            {
+                GetComponent<AudioSource>().Play();
+                Debug.Log("collision!");
+                Teleport0();
+            }
+        }
+
+     
+        if (target.gameObject.CompareTag("ToCheckpoint1"))
+        {
+
+            GetComponent<AudioSource>().Play();
+            Debug.Log("collision!");
+            Teleport1();
+        }
+
+        if (target.gameObject.CompareTag("ToCheckpoint2"))
+        {
+
+            GetComponent<AudioSource>().Play();
+            Debug.Log("collision!");
+            Teleport2();
+        }
+
+        if (target.gameObject.CompareTag("ToCheckpoint3"))
+        {
+
+            GetComponent<AudioSource>().Play();
+            Debug.Log("collision!");
+            Teleport3();
+        }
     }
     void DamageControl()
     {
-        _healthControl.health = _healthControl.health - obstacleDamage;
-        _healthControl.UpdateHealth();
-        gameObject.SetActive(false);
+        _gameManager.GetComponent<GameManagerScript>().health = _gameManager.GetComponent<GameManagerScript>().health - obstacleDamage;
+        Debug.Log(_gameManager.GetComponent<GameManagerScript>().health);
+        _gameManager.GetComponent<GameManagerScript>().UpdateHealth();
+        //gameObject.SetActive(false);
+    }
+
+    void Teleport0()
+    {
+        Player.transform.position = ToCheckpoint0.transform.position;
+    }
+    void Teleport1()
+    {
+        Player.transform.position = ToCheckpoint1.transform.position;
+    }
+    void Teleport2()
+    {
+        Player.transform.position = ToCheckpoint2.transform.position;
+    }
+    void Teleport3()
+    {
+        Player.transform.position = ToCheckpoint3.transform.position;
     }
 }
--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour
 {
+    // Variable for level manager
     public int level = 0;
+
+    // Variables for health system
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        UpdateHealth();
     }
 
     // Update is called once per frame
@@ -28,5 +39,39 @@ public class GameManagerScript : MonoBehaviour
         level = level + 1;
     }
 
+    public void UpdateHealth()
+    {
+        /*if (health <= 0)
+        {
+            // Restart previous scene
+        }*/
+
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                //if health is zero = gameover + restart previous level with full health
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
 
 }
